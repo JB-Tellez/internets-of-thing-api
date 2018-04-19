f49a5b76fe9f2bc13bac8936f8577855c78d2035
JB Tellez <jb.tellez@gmail.com>
2018-04-19 10:09:35 -0700

    adds rest of CRUD routes

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.get('/api/v1/things', async (req, res) => {
     
     client.query('SELECT * FROM things;')
@@ -24,6 +27,36 @@ app.get('/api/v1/things/:id', async (req, res) => {
         .then(results => res.send(results.rows[0]))
         .catch(err => res.status(500).send('ruh roh'));
 
+});
+
+app.post('/api/v1/things', (req, res) => {
+
+    client.query(`
+        INSERT INTO things (name) 
+        VALUES ('${req.body.name}');
+    `)
+    .then(results => res.send(results.rows[0]))
+    .catch(err => res.status(500).send('ruh roh'));
+});
+
+app.put('/api/v1/things', (req, res) => {
+
+    client.query(`
+        UPDATE things 
+        SET name='${req.body.name}' 
+        WHERE id=${req.body.id};
+    `)
+    .then(results => res.sendStatus(200))
+    .catch(err => res.status(500).send('ruh roh'));
+});
+
+app.delete('/api/v1/things/:id', (req, res) => {
+    client.query(`
+    
+        DELETE FROM things WHERE id=${req.params.id};
+    `)
+    .then(results => res.sendStatus(200))
+    .catch(err => res.status(500).send('ruh roh'));
 });
 
 app.listen(PORT, () => console.log('Listening on PORT', PORT));
