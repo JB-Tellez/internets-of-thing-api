ac4950536537e623cf42b8bb76ac9ca660522473
JB Tellez <jb.tellez@gmail.com>
2018-04-19 01:48:03 -0700

    Adds GET things route

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ require('dotenv').config();
 const express = require('express');
 
 const PORT = process.env.PORT;
+const pg = require('pg');
+
 
 const app = express();
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
 
-app.get('/ping', (req, res) => res.send('pong'));
+app.get('/api/v1/things', async (req, res) => {
+    
+    client.query('SELECT * FROM things')
+        .then(results => res.send(results.rows))
+        .catch(err => res.status(500).send('ruh roh'));
+
+})
 
 app.listen(PORT, () => console.log('Listening on PORT', PORT));
